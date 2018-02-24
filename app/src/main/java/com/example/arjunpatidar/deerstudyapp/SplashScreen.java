822ec0101326603821d45ceccd251f617e4fecfd
arjun <arjunpatidar952@gmail.com>
2018-02-24 09:15:33 +0530

    test 1

--- app/src/main/java/com/example/arjunpatidar/deerstudyapp/SplashScreen.java
+++ app/src/main/java/com/example/arjunpatidar/deerstudyapp/SplashScreen.java
@@ -19,7 +19,11 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
  // hii arjun patidar
+
+
         imageView = (ImageView)findViewById(R.id.imageView);
         if(imageView == null) throw new AssertionError();
         imageView.setBackgroundResource(R.drawable.deer_animation);
