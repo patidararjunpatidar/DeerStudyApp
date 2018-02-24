c83d96925eed5e54df489a5a31ce6a9f3cdd6e5b
arjun <arjunpatidar952@gmail.com>
2018-02-24 08:58:55 +0530

    test

---
+++ app/src/main/java/com/example/arjunpatidar/deerstudyapp/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.arjunpatidar.deerstudyapp;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    ImageView imageView;
+    AnimationDrawable anim;
+    private  static  int SPLASH_TIME_OUT =2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        imageView = (ImageView)findViewById(R.id.imageView);
+        if(imageView == null) throw new AssertionError();
+        imageView.setBackgroundResource(R.drawable.deer_animation);
+
+        anim = (AnimationDrawable)imageView.getBackground();
+
+        anim.start();
+
+    }
+}
