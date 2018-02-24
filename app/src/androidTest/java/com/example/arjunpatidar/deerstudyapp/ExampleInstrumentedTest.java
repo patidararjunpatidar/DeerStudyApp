2228b89b1eb603c836008b70f63d704523f1652d
arjun <arjunpatidar952@gmail.com>
2018-02-24 19:45:18 +0530

    test 4

--- app/src/androidTest/java/com/example/arjunpatidar/deerstudyapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/arjunpatidar/deerstudyapp/ExampleInstrumentedTest.java
@@ -18,8 +18,14 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
+
+
         // Context of the app under test.
+
+
         Context appContext = InstrumentationRegistry.getTargetContext();
+
+        // my second change
 
         assertEquals("com.example.arjunpatidar.deerstudyapp", appContext.getPackageName());
     }
